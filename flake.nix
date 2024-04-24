1af3ebce6b391ad7633bc865759d163e26b8e55a
Max Tyler <maxastyler@gmail.com>
2024-04-24 18:52:14 +0100

    Initialise cargo

--- flake.nix
+++ flake.nix
@@ -1,34 +1,3 @@
-# {
-#   inputs = {
-#     fenix = {
-#       url = "github:nix-community/fenix";
-#       inputs.nixpkgs.follows = "nixpkgs";
-#     };
-#     nixpkgs.url = "nixpkgs/nixos-unstable";
-#   };
-
-#   outputs = { self, fenix, nixpkgs }: {
-#     packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
-#     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-#       system = "x86_64-linux";
-#       modules = [
-#         ({ pkgs, ... }: {
-#           nixpkgs.overlays = [ fenix.overlays.default ];
-#           environment.systemPackages = with pkgs; [
-#             (fenix.complete.withComponents [
-#               "cargo"
-#               "clippy"
-#               "rust-src"
-#               "rustc"
-#               "rustfmt"
-#             ])
-#             rust-analyzer-nightly
-#           ];
-#         })
-#       ];
-#     };
-#   };
-# }
 {
   description = "A flake with a rust environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";

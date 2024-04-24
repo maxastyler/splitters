c1965a122ae2a67ea3ede3de7c27c592ded5606a
Max Tyler <maxastyler@gmail.com>
2024-04-24 18:46:02 +0100

    Add rust-toolchain

--- flake.nix
+++ flake.nix
@@ -1,31 +1,81 @@
-{
-  inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
+# {
+#   inputs = {
+#     fenix = {
+#       url = "github:nix-community/fenix";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#     nixpkgs.url = "nixpkgs/nixos-unstable";
+#   };
 
-  outputs = { self, fenix, nixpkgs }: {
-    packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({ pkgs, ... }: {
-          nixpkgs.overlays = [ fenix.overlays.default ];
-          environment.systemPackages = with pkgs; [
-            (fenix.complete.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-            rust-analyzer-nightly
-          ];
-        })
-      ];
-    };
-  };
+#   outputs = { self, fenix, nixpkgs }: {
+#     packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
+#     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+#       system = "x86_64-linux";
+#       modules = [
+#         ({ pkgs, ... }: {
+#           nixpkgs.overlays = [ fenix.overlays.default ];
+#           environment.systemPackages = with pkgs; [
+#             (fenix.complete.withComponents [
+#               "cargo"
+#               "clippy"
+#               "rust-src"
+#               "rustc"
+#               "rustfmt"
+#             ])
+#             rust-analyzer-nightly
+#           ];
+#         })
+#       ];
+#     };
+#   };
+# }
+{
+  description = "A flake with a rust environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+        libPath = with pkgs; lib.makeLibraryPath [
+          # load external libraries that you need in your rust project here
+        ];
+      in
+        {
+          devShells.default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              clang
+              # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
+              llvmPackages.bintools
+              rustup
+            ];
+            RUSTC_VERSION = overrides.toolchain.channel;
+            # https://github.com/rust-lang/rust-bindgen#environment-variables
+            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+            shellHook = ''
+      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+      '';
+            # Add precompiled library to rustc search path
+            RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+              # add libraries here (e.g. pkgs.libvmi)
+            ]);
+            LD_LIBRARY_PATH = libPath;
+            # Add glibc, clang, glib, and other headers to bindgen search path
+            BINDGEN_EXTRA_CLANG_ARGS =
+              # Includes normal include path
+              (builtins.map (a: ''-I"${a}/include"'') [
+                # add dev libraries here (e.g. pkgs.libvmi.dev)
+                pkgs.glibc.dev
+              ])
+              # Includes with special directory paths
+              ++ [
+                ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+                ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+                ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+              ];
+          };
+        });
 }

4635b0187aa09f6a122968681ee0c697cb9abeba
Max Tyler <maxastyler@gmail.com>
2024-04-24 19:24:48 +0100

    Update flake with sqlx

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
               # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
               llvmPackages.bintools
               rustup
+              sqlx-cli
             ];
             RUSTC_VERSION = overrides.toolchain.channel;
             # https://github.com/rust-lang/rust-bindgen#environment-variables
